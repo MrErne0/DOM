7d15a34bfcacc1c92e6bb761d982898b14a2f301
MrErne0 <ernesto1.diaz2@gmail.com>
2022-01-23 22:09:26 -0600

    DOM salior team

---
+++ DOM.js
@@ -0,0 +1,7 @@
+var formularios = document.getElementsByTagName('form');
+var primerFormulario = formularios[0];
+var segundoFormulario = formularios[1];
+
+console.log(primerFormulario);
+
+console.log(segundoFormulario);
